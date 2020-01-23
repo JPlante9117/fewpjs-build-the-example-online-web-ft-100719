3c2dee69d1e0572a137443c7b8a49cd0c29051da
JPlante9117 <jacrplante@gmail.com>
2020-01-23 14:52:23 -0500

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let errorPresent = false
 
 // Your JavaScript code goes here!
+
+  let modal = document.getElementById('modal')
+  let hearts = document.querySelectorAll('.like')
+  modal.setAttribute('class', 'hidden')
+
+  const errorHandler = () =>{
+    errorPresent = !errorPresent
+  
+    if (errorPresent){
+      modal.setAttribute('class', '')
+    } else{
+      modal.setAttribute('class', 'hidden')
+    }
+  }
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () =>{
+    return mimicServerCall()
+      .then(() => {
+        if (heart.lastElementChild.className == 'like-glyph'){
+          let heartSpan = heart.querySelector('.like-glyph')
+          let glyph = heartSpan.textContent
+          heartSpan.textContent = swapGlyph(glyph)
+          heartSpan.setAttribute('class', 'activated-heart')
+        } else {
+          let heartSpan = heart.querySelector('.activated-heart')
+          let glyph = heartSpan.textContent
+          heartSpan.textContent = swapGlyph(glyph)
+          heartSpan.setAttribute('class', 'like-glyph')
+        }
+      })
+      .catch(error => {
+        errorHandler()
+        let pTag = modal.querySelector('#modal-message')
+        pTag.textContent = error
+        setTimeout(() => errorHandler(), 5000)
+      })
+    })
+  })
+
+  const swapGlyph = glyph =>{
+    if (glyph == EMPTY_HEART){
+      return glyph = FULL_HEART
+    } else {
+      return glyph = EMPTY_HEART
+    }
+  }
 
 
 
